f9347b5cea8156f93f913831700593fbd6e6a935
yarepka <yarepka01@gmail.com>
2018-10-30 13:40:17 +0300

    Spring MVC + Spring boot + Thymeleaf

--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -4,5 +4,4 @@ import guru.springframework.spring5webapp.model.Author;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AuthorRepository extends CrudRepository<Author, Long> {
-
 }
